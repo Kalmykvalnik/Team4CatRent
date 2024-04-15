593efa07e414c7571ed4830f188247aaf8ac3cd7
soullaft <soullaft@gmail.com>
2024-04-15 15:29:41 +0400

    moved front to frontend, added backend

---
+++ Backend/src/main.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import * as bodyParser from 'body-parser';
+import routes from './app/routes/routes';
+import HttpException from './app/models/http-exception.model';
+
+const app = express();
+
+/**
+ * App Configuration
+ */
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
+
+// Serves images
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.json({ status: 'API is running on /api' });
+});
+
+app.use(
+  (
+    err: Error | HttpException,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    // @ts-ignore
+    if (err && err.name === 'UnauthorizedError') {
+      return res.status(401).json({
+        status: 'error',
+        message: 'missing authorization credentials',
+      });
+      // @ts-ignore
+    } else if (err && err.errorCode) {
+      // @ts-ignore
+      res.status(err.errorCode).json(err.message);
+    } else if (err) {
+      res.status(501).json(err.message);
+    }
+  }
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.info(`server up on port ${PORT}`);
+});
