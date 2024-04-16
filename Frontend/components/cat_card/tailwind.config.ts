a98ea5c06d228bcfd7bd6e042412d12c57b1c4de
Kulik Oleg <kulikolval@gmail.com>
2024-04-16 12:52:31 +0700

    work on components

--- Frontend/components/cat_card/tailwind.config.ts
+++ Frontend/components/cat_card/tailwind.config.ts
@@ -13,8 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      listStyleImage: {
+        checkmark: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>',
+      },
     },
-  },
+  },/*
+  daisyui : {
+    themes : [
+      {
+        mttheme : {
+
+        }
+      }
+    ]
+  },*/
   plugins: [require("daisyui")],
 };
 export default config;
