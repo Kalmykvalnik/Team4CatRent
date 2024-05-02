b43146ec90adc4c2f13957093a4fac6cdf500747
Kulik Oleg <kulikolval@gmail.com>
2024-05-02 12:27:59 +0700

    carousel done

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -10,18 +10,18 @@ const mainCat1 = [
 {
   name: "Близнецы",
   shortDescription: "Ласковая и игривая парочка которая не заставит вас скучать",
-  imgPrew: ""
+  imgPrew: "https://t4.ftcdn.net/jpg/02/66/72/41/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg"
 },
 
 {
   name: "Добряк",
   shortDescription: "Большой и добрый мэйн-кун скрасит ваши выходные",
-  imgPrew: ""
+  imgPrew: "https://t3.ftcdn.net/jpg/02/36/99/22/240_F_236992283_sNOxCVQeFLd5pdqaKGh8DRGMZy7P4XKm.jpg"
 },
 {
   name: "Красотка",
   shortDescription: "Кокетка захватит ваше сердце и не оставит равнодушным никого",
-  imgPrew: ""
+  imgPrew: "https://t3.ftcdn.net/jpg/02/97/99/50/240_F_297995004_KIRJlI4JXoOGdnk6fj8NMTC9Xvt7b5Im.jpg"
 }
 ]
 export default function Home() {
--- Frontend/src/components/common/carousel.tsx
+++ Frontend/src/components/common/carousel.tsx
@@ -11,49 +11,52 @@ import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 
 type catShortInfo = {
-    name: string;
-    shortDescription: string;
-    imgPrew: string;
+  name: string;
+  shortDescription: string;
+  imgPrew: string;
 }
 
 type props = {
-    catsShortInfo: catShortInfo[]
+  catsShortInfo: catShortInfo[]
 }
 
 const Carousel = ({ catsShortInfo }: props) => {
 
-    return <Swiper
-        navigation
-        pagination={{ type: 'fraction' }}
-        modules={[Navigation]}
-        slidesPerView={2}
-        className='flex max-w-[1100px] justify-center gap-4'
-    >
-        {catsShortInfo.map((cat, index) => {
-            return (
-                <SwiperSlide key={index} className='flex max-w-[1100px] justify-center'>
-                    <div className='border-solid border-2 border-black flex max-w-[520px] min-h-[270px] max-h-[500px]'>
-                        <div className='flex'>
-                            <div className='p-2'>
-                                <Image height={230} width={287} src="" alt="cat_photo"></Image>
-                            </div>
-                            <div className='grid'>
-                                <h1 className='text-center p-2 flex justify-center self-center'>{cat.name}</h1>
-                                <span className='p-2'>{cat.shortDescription}</span>
-                                <div className='flex self-center justify-center h-[40px]'>
-                                    <button className="bg-[#0F57C2] hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-lg p-2  ">Выбрать</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
+  return <Swiper
 
-            )
-        })
-        }
-    </Swiper>
+    pagination={{ type: 'bullets' }}
+    modules={[Pagination]}
+    slidesPerView={2}
 
+    spaceBetween={20}
+    grabCursor={true}
+    className='flex max-w-[1040px] justify-center min-h-[300px]'
+  >
+    {catsShortInfo.map((cat, index) => {
+      return (
+        <SwiperSlide key={index} className='flex max-w-[1100px] justify-center '>
+          <div className='border-solid border-2 border-black flex max-w-[520px] min-h-[270px] justify-between '>
+            <div className='flex  gap-4'>
+              <div className='flex p-2 justify-items-center'>
+                <Image src={cat.imgPrew} alt="cat_photo" height={230} width={287} className='flex object-cover min-h-[230px]'></Image>
+              </div>
+              <div className='flex max-w-[156px]'>
+                <div className='grid'>
+                  <h1 className='text-center p-2 flex justify-center self-center'>{cat.name}</h1>
+                  <span className=''>{cat.shortDescription}</span>
+                  <div className='flex self-center justify-center h-[40px]'>
+                    <button className="bg-[#0F57C2] hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-lg p-2  ">Выбрать</button>
+                  </div>
+                </div>
+              </div>
 
+            </div>
+          </div>
+        </SwiperSlide>
+      )
+    })
+    }
+  </Swiper>
 }
 
 export { Carousel }
