8788e138feaadafe49cb78c493116abd11a2f535
soullaft <soullaft@gmail.com>
2024-05-08 18:07:35 +0400

    reworked auth controller

--- Backend/src/app/routes/auth/auth.controller.ts
+++ Backend/src/app/routes/auth/auth.controller.ts
@@ -1,18 +1,25 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { createUser } from './auth.service';
 
 const router = Router();
 
 //register
 router.post(
-  '/user',
+  '/register',
   async (req: Request, res: Response, next: NextFunction) => {
-    res.json('Basket');
+    try {
+      console.log(req.body);
+      const user = await createUser({ ...req.body });
+      res.status(201).json(user);
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
 //login
 router.post(
-  '/user/login',
+  'login',
   async (req: Request, res: Response, next: NextFunction) => {
     res.json('Basket');
   }
