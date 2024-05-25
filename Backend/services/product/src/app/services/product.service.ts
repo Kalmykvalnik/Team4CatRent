dc7d4b0d3f751375ad0f10907488a0a35b3a7e58
Alexande <tarasov.sasha@list.ru>
2024-05-25 22:36:10 +0300

    crud fix

--- Backend/services/product/src/app/services/product.service.ts
+++ Backend/services/product/src/app/services/product.service.ts
@@ -3,13 +3,13 @@ import prisma from '../../prisma/prisma-client';
 const productsperpage: number = 20;
 
 export const getAllProducts = async (pagenumber: number) => {
-  let products: any;
+  let products: Product;
   if (pagenumber === 1) {
-    products = await prisma.product.findMany({
+    [products] = await prisma.product.findMany({
       take: productsperpage,
     });
   } else {
-    products = await prisma.product.findMany({
+    [products] = await prisma.product.findMany({
       skip: pagenumber * productsperpage,
       take: productsperpage,
     });
@@ -18,32 +18,21 @@ export const getAllProducts = async (pagenumber: number) => {
 };
 
 export const getProduct = async (id: number) => {
-  const product = await prisma.product.findFirst({
-    where: {
-      id: id,
-    },
-  });
+  const product:Product|null = await prisma.product.findFirst({where:{id}});
   return product;
 };
 
 export const deleteProduct = async (id:number)=>{
-    const product = await prisma.product.delete({
-      where:{
-        id: id,
-      }
-    })
+  const product:Product = await prisma.product.delete({where:{id}});
   return product;
 }
 
 export const editProduct = async (id: number, data:Product)=>{
-  const product = await prisma.product.update({
-    where: {id}, 
-    data: data //orderList??
-  });
+  const product:Product = await prisma.product.update({where: {id}, data});
   return product;
 }
 
 export const createProduct = async (data:Product)=>{
-  const product = await prisma.product.create({data});
+  const product:Product = await prisma.product.create({data});
   return product;
 }
